3fcfbf32ccdb50bb9b1079bbfdac98929919d383
Isaac Pendergrass <isaac.pendergrass@gmail.com>
2023-05-18 09:13:07 -0700

    Added isEmpty to stack. Added input checks to Peek and Pop.

--- stack/stack.go
+++ stack/stack.go
@@ -1,10 +1,16 @@
 package stack
 
+import (
+	"errors"
+)
+
 type (
 	stack[T any] struct {
 		store []T
 	}
 )
+
+var stackEmpty = "stack is empty"
 
 func NewStack[T any]() *stack[T] {
 	return &stack[T]{
@@ -14,14 +20,24 @@ func NewStack[T any]() *stack[T] {
 
 func (s stack[T]) Height() int { return len(s.store) }
 
-func (s *stack[T]) Pop() T {
+func (s stack[T]) IsEmpty() bool { return len(s.store) == 0 }
+
+func (s stack[T]) Peek() (*T, error) {
+	if !s.IsEmpty() {
+		return &s.store[0], nil
+	}
+	return nil, errors.New(stackEmpty)
+}
+
+func (s *stack[T]) Pop() (*T, error) {
+	if s.IsEmpty() {
+		return nil, errors.New(stackEmpty)
+	}
 	top := s.store[0]
 	s.store = s.store[1:]
-	return top
+	return &top, nil
 }
 
 func (s *stack[T]) Push(t T) {
 	s.store = append([]T{t}, s.store...)
 }
-
-func (s *stack[T]) Peek() T { return s.store[0] }
